baca8c15d54d065ecede545b62c5b137a87c76c1
timmie1337 <timmie99@live.se>
2017-01-19 10:48:21 +0100

    added Hello world

---
+++ Hello_World/Hello_World/MyProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class MyProgram
+    {
+        public void run()
+        {
+            HelloWorld();
+        }
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+            
+      
+
+
+
+    }
+}
